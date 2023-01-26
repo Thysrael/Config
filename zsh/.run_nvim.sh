39cd15b94e0c148fe084630025bf810c801fd769
Thysrael <2470583476@qq.com>
2023-01-26 18:57:46 +0800

    neovim 字体设置

--- zsh/.run_nvim.sh
+++ zsh/.run_nvim.sh
@@ -1,2 +1,2 @@
 # 这个脚本用于在一个新的终端里启动 neovim
-konsole --hide-menubar --hide-tabbar -e zsh -c "nvim $1" &>/dev/null& 
+konsole --hide-menubar --hide-tabbar --profile neovim -e zsh -c "nvim $1" &>/dev/null& 
